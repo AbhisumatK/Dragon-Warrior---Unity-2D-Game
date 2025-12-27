2a77e09b06781dfe63eefb89091f55714dc3567e
unknown <kunduabhisumat@gmail.com>
2025-12-27 16:11:11 +0530

    Added Melee enemies and corresponding animations

---
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private int damage;
+
+    private void Update()
+    {
+        // Melee enemy behavior implementation
+    }
+}
