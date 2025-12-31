b1f091f08710a3bc4fc1d7cc98705d7d6cff1ecf
unknown <kunduabhisumat@gmail.com>
2025-12-31 20:30:00 +0530

    Make Enemy Patrol script

--- Assets/Scripts/Enemy/EnemyPatrol.cs
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Patrol Points")]
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+    [Header("Enemy")]
+    [SerializeField] private Transform enemy;
+
+    [Header("Movement Points")]
+    [SerializeField] private float speed;
+    private Vector3 initScale;
+    private bool movingLeft;
+
+    private void Awake()
     {
-        
+        initScale = enemy.localScale;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (movingLeft)
+        {
+            moveInDirection(-1);
+        }
+        else
+        {
+            moveInDirection(1);
+        }
+    }
+
+    private void moveInDirection(int _direction)
+    {
+        // Movement logic here
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x * _direction), initScale.y, initScale.z);
+
+        //move in the given direction
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed, enemy.position.y, enemy.position.z);
     }
 }
