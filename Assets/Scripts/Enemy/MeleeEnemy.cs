6980288105ee710ad4cf4bf11ec1b839b439d7c9
unknown <kunduabhisumat@gmail.com>
2025-12-31 18:13:08 +0530

    Added melee enemy box collider

--- Assets/Scripts/Enemy/MeleeEnemy.cs
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -4,9 +4,35 @@ public class MeleeEnemy : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
     [SerializeField] private int damage;
+    [SerializeField] private float range;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float coolDownTimer = Mathf.Infinity;
 
     private void Update()
     {
-        // Melee enemy behavior implementation
+        coolDownTimer += Time.deltaTime;
+        // attack when player in sight
+        if (PlayerInSight())
+        {
+            if(coolDownTimer >= attackCooldown)
+            {
+                //Attack player
+                
+            }
+        }
+        
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 }
