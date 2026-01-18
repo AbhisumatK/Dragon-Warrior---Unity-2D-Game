f4927da0ba8c1512286ac0cfe5028bd8a1e18820
unknown <kunduabhisumat@gmail.com>
2026-01-18 20:26:58 +0530

    tried to make Main Menu UI

---
+++ Assets/Scripts/Ui/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
