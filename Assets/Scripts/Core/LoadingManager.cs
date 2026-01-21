265ac9eecb92c1fb1c578d987ecf56b8555ad4b0
unknown <kunduabhisumat@gmail.com>
2026-01-21 13:24:55 +0530

    added main menu music

--- Assets/Scripts/Core/LoadingManager.cs
+++ Assets/Scripts/Core/LoadingManager.cs
@@ -1,13 +1,1 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class LoadingManager : MonoBehaviour
-{
-    // void Update()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.F))
-    //     {
-    //         SceneManager.LoadScene(1); // Load scene with build index 2
-    //     }
-    // }
-}
---
+++ Assets/Scripts/Ui/MenuVolumeSettings.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class MenuVolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider soundSlider;
+
+    private void Start()
+    {
+        float savedMusicVolume = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        musicSlider.value = savedMusicVolume;
+        SetMusicVolume(savedMusicVolume);
+
+        float savedSoundVolume = PlayerPrefs.GetFloat("SoundVolume", 1f);
+        soundSlider.value = savedSoundVolume;
+        SetSoundVolume(savedSoundVolume);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        musicSource.volume = value;
+        PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+    public void SetSoundVolume(float value)
+    {
+        musicSource.volume = value;
+        PlayerPrefs.SetFloat("SoundVolume", value);
+    }
+}
